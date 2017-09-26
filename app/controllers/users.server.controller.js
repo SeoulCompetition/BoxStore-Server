9dfe7b01bf7913ddea1a12b65fd30314e1b2657a
rlatjdwn4926 <seongjoo.kim@goorm.io>
2017-09-26 06:05:41 +0000

    id unique 추가

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -18,7 +18,7 @@ exports.list = function(req,res,next){
     }else{
       res.json(users);
     }
-  })
+  });
 };
 
 exports.read = function(req,res){
--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,15 +1,31 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var UserSchema = new Schema({
-  username : {
-    type : String,
-    trim : true,
-    required : true
-  }
+	user_id :{
+		unique : true,
+		type : String,
+		trim : true,
+		require : true,
+	},
+	password : {
+		type : String,
+		trim : true,
+		require : true  
+  	},
+	user_name : {
+		type : String,
+		trim : true,
+		require : true  
+  	},
+	phone_number : {
+		type : String,
+		trim : true
+	}
+	
 });
 
 UserSchema.virtual('idpass').get(function(){
-  return this.userid + ' ' + this.password;
+  return this.user_id + ' ' + this.password;
 });
 
 UserSchema.set('toJSON',{ getters : true , virtuals : true});
