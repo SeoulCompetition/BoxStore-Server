cc5c146e076c2c233200e0a1b7f49b10f08b008f
boxstore-server <ec2-user@ip-10-0-1-70.ap-northeast-2.compute.internal>
2017-10-17 06:06:04 +0000

    category query 수정

--- app/controllers/categories.server.controller.js
+++ app/controllers/categories.server.controller.js
@@ -3,21 +3,22 @@ var MongoClient = require("mongodb").MongoClient;
 
 exports.list = function(req, res, next) {
     var category = req.params.category_id || null;
+    var filter = {
+                _id: 0,
+                name: 1,
+                parent: 1,
+                description : 1
+        };
     MongoClient.connect("mongodb://localhost:27017/seoul-competition", function(err, db) {
         db.collection("categories").findOne({"name" : category},
-	{
-		"_id": false,
-		"name": true,
-		"parent": true,
-		"description" : true
-	}).lean().exec(function (err,result) {
+	filter,function (err,result) {
           if(result.index == 2 ){
             res.json({
               "RESULT" : "SELECTED",
               "DATA" : category
             });
           }else {
-            db.collection("categories").find({"parent":category}).toArray(function(err, result) {
+            db.collection("categories").find({"parent":category},filter).toArray(function(err, result) {
                 if (err) {
                     res.json({
                         "RESULT": "ERR",
