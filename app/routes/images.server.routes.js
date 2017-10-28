304f386d9f2c77f6e85038271168680f0ab33b91
Loloara <addy94@naver.com>
2017-10-28 22:59:49 +0900

    set file directory

--- app/routes/images.server.routes.js
+++ app/routes/images.server.routes.js
@@ -1,11 +1,10 @@
 var images = require('../../app/controllers/images.server.controller');
 var multer = require('multer');
-var upload = multer({dest: 'uploads/'});
+var upload = multer({dest: 'config/public/original/'});
 
 module.exports = function(app){
   app.route('/stuffs/images/:stuffId')
-    .post(upload.array('photos', 8), images.uploadForStuff);
+    .post(upload.array('photo', 8), images.uploadForStuff);
   app.route('/receipt/images/:stuffId')
-    .post(images.uploadForReceipt);
-
+    .post(upload.single('photo'), images.uploadForReceipt);
 };
