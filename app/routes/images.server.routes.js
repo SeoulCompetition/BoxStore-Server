542e6b50f7615bff3f9c1cdcc2a83036795d5dd9
boxstore-server <ec2-user@ip-10-0-1-70.ap-northeast-2.compute.internal>
2017-10-28 15:03:27 +0000

    fix typo

--- app/routes/images.server.routes.js
+++ app/routes/images.server.routes.js
@@ -4,7 +4,7 @@ var upload = multer({dest: 'uploads/'});
 
 module.exports = function(app){
   app.route('/stuffs/images/:stuffId')
-    .post(upload.array('photos', 8), images.uploadForStuff);
+    .post(upload.array('photo', 8), images.uploadForStuff);
   app.route('/receipt/images/:stuffId')
     .post(images.uploadForReceipt);
 
