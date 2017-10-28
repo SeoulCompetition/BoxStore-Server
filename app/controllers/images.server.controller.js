e5f3ded703dc49165a4f74bf8fb4e63e8f37242d
Loloara <addy94@naver.com>
2017-10-28 19:52:56 +0900

    post _id String

--- app/controllers/images.server.controller.js
+++ app/controllers/images.server.controller.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var sharp = require('sharp');
+//var sharp = require('sharp');
 
 var IMAGE_PATH = './config/public/original/';
 var THUMBNAIL_PATH = './config/public/thumbnails/';
@@ -7,8 +7,8 @@ var IMAGE_URL = 'http://localhost:3000' + '/original/';
 var THUMBNAILS_URL = 'http://localhost:3000' + '/thumbnails/';
 var WIDE_SIZE = 100;
 
-exports.upload = function(req, res){
   return new Promise((resolve, reject) => {
+    exports.upload = function(req, res){
     var buf = new Buffer(req.body.img.data, 'base64');
     var imageName = req.body.name;    //'image.jpg'
 
@@ -16,7 +16,7 @@ exports.upload = function(req, res){
       if(err) console.log('writeFile error: ' + err);
       else{
         console.log('success to upload image: ' + imageName);
-        resizeImage(imageName);
+  //      resizeImage(imageName);
         var urlArray = [IMAGE_URL+imageName, THUMBNAILS_URL + imageName.split('.')[0] + '.png'];
         console.log(urlArray);
         resolve(urlArray);  //urlArray[0] -> raw_img_urls, urlArray[1] -> resized_img_urls
@@ -25,6 +25,30 @@ exports.upload = function(req, res){
   });
 };
 
+exports.uploadForStuff = function(req, res){
+  console.log(req);
+  res.json(req);
+}
+
+exports.uploadForReceipt = function(req, res){
+  return new Promise((resolve, reject) => {
+    var buf = new Buffer(req.body.img.data, 'base64');
+    var imageName = req.body.name;    //'image.jpg'
+
+    fs.writeFile(IMAGE_PATH + imageName, buf, 'base64', function(err){
+      if(err) console.log('writeFile error: ' + err);
+      else{
+        console.log('success to upload image: ' + imageName);
+//        resizeImage(imageName);
+        var urlArray = [IMAGE_URL+imageName, THUMBNAILS_URL + imageName.split('.')[0] + '.png'];
+        console.log(urlArray);
+        resolve(urlArray);  //urlArray[0] -> raw_img_urls, urlArray[1] -> resized_img_urls
+      }
+    });
+  });
+};
+
+/**
 //imageName: 'image.jpg'
 function resizeImage(imageName){
   sharp(IMAGE_PATH + imageName)
@@ -37,3 +61,4 @@ function resizeImage(imageName){
     }
   });
 };
+**/
